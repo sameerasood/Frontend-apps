1b99e479cd2a5da2d77cd143f8f4814f9a926eae
sameera sood <sameerasood@gmail.com>
2023-02-27 16:26:41 +0000

    log console messages

--- notes-app/index.js
+++ notes-app/index.js
@@ -1,1 +1,7 @@
-console.log("The notes app is running");
+const NotesModel = require("./notesModel.js");
+const model = new NotesModel();
+
+console.log(model.addNotes("Buy milk"));
+console.log(model.addNotes("Go to the gym"));
+const notes = model.getNotes();
+console.log(notes);
